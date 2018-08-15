f28aba8dcdf6bd3b5319755ac0d090316b26d75b
jamesarems <jamesarems@gmail.com>
2018-08-15 13:04:20 +0000

    New Patch

--- plan_limit.sh
+++ plan_limit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+## GIThub : https://github.com/jamesarems
+## Email : info@jamesarems.com
+##########################################################
+# Execute scripts upon requested by server
 /bin/bash /root/VPN-SERVER-SCRIPTS/rate_limit.sh reset
 /bin/bash /root/VPN-SERVER-SCRIPTS/rate_limit.sh setup
 /bin/bash /root/VPN-SERVER-SCRIPTS/plan_script/a1.sh
